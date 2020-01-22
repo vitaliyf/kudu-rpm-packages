f6ad0f94b4482ae4f3eecb5a4e29e34c40c4b72d
Vitaliy Fuks <vitaliy.fuks@upsight.com>
2020-01-22 00:01:14 +0000

    Initial commit based on CDH 5.16.2 (Kudu 1.7).
    Forked from:
    - https://github.com/cloudera/cdh-package/tree/cdh5-0.6.0_5.16.2/bigtop-packages/src/common/kudu
    - https://github.com/cloudera/cdh-package/tree/cdh5-0.6.0_5.16.2/bigtop-packages/src/rpm/kudu

---
+++ install_kudu.sh
@@ -0,0 +1,262 @@
+#!/bin/bash -x
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+usage() {
+  echo "
+usage: $0 <options>
+  Required not-so-options:
+     --build-dir=DIR             path to Kudu build directory
+     --prefix=PREFIX             path to install into
+     --extra-dir=DIR             path to extra packaging files
+
+  Optional options:
+     --bin-dir=DIR               path to install bins [/usr/bin]
+     --lib-dir=DIR               path to install libraries [/usr/lib/kudu]
+     --sbin-dir=DIR              path to install system bins [/usr/sbin]
+     --doc-dir=DIR               path to install docs [/usr/share/doc/kudu]
+     --man-dir=DIR               path to install manpages [/usr/man]
+     --conf-dir=DIR              path to install default configuration [/etc/kudu/conf.dist]
+     --system-include-dir=DIR    path to install system include files [/usr/include]
+     --system-lib-dir=DIR        path to install system libraries [/usr/lib]
+  "
+  exit 1
+}
+
+OPTS=$(getopt \
+  -n $0 \
+  -o '' \
+  -l 'prefix:' \
+  -l 'build-dir:' \
+  -l 'lib-dir:' \
+  -l 'bin-dir:' \
+  -l 'sbin-dir:' \
+  -l 'doc-dir:' \
+  -l 'man-dir:' \
+  -l 'conf-dir:' \
+  -l 'system-include-dir:' \
+  -l 'system-lib-dir:' \
+  -l 'extra-dir:' \
+  -- "$@")
+
+if [ $? != 0 ] ; then
+    usage
+fi
+
+eval set -- "$OPTS"
+while true ; do
+    case "$1" in
+        --prefix)
+        PREFIX=$2 ; shift 2
+        ;;
+        --build-dir)
+        BUILD_DIR=$2 ; shift 2
+        ;;
+        --lib-dir)
+        LIB_DIR=$2 ; shift 2
+        ;;
+        --bin-dir)
+        BIN_DIR=$2 ; shift 2
+        ;;
+        --sbin-dir)
+        SBIN_DIR=$2 ; shift 2
+        ;;
+        --man-dir)
+        MAN_DIR=$2 ; shift 2
+        ;;
+        --doc-dir)
+        DOC_DIR=$2 ; shift 2
+        ;;
+        --conf-dir)
+        CONF_DIR=$2 ; shift 2
+        ;;
+        --system-include-dir)
+        SYSTEM_INCLUDE_DIR=$2 ; shift 2
+        ;;
+        --system-lib-dir)
+        SYSTEM_LIB_DIR=$2 ; shift 2
+        ;;
+        --extra-dir)
+        EXTRA_DIR=$2 ; shift 2
+        ;;
+        --)
+        shift ; break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+for var in PREFIX BUILD_DIR; do
+  if [ -z "$(eval "echo \$$var")" ]; then
+    echo Missing param: $var
+    usage
+  fi
+done
+
+ETC_DIR=${ETC_DIR:-$PREFIX/etc}
+LIB_DIR=${LIB_DIR:-$PREFIX/usr/lib/kudu}
+BIN_DIR=${BIN_DIR:-$PREFIX/usr/bin}
+SBIN_DIR=${SBIN_DIR:-$PREFIX/usr/sbin}
+DOC_DIR=${DOC_DIR:-$PREFIX/usr/share/doc/kudu}
+MAN_DIR=${MAN_DIR:-$PREFIX/usr/man}
+CONF_DIR=${CONF_DIR:-$PREFIX/etc/kudu/conf.dist}
+SYSTEM_INCLUDE_DIR=${SYSTEM_INCLUDE_DIR:-/usr/include}
+SYSTEM_LIB_DIR=${SYSTEM_LIB_DIR:-/usr/lib}
+
+install -d -m 0755 ${LIB_DIR}
+
+# install the executables
+bin_executables="kudu"
+install -d -m 0755 ${LIB_DIR}/bin-release
+install -d -m 0755 ${LIB_DIR}/bin-debug
+for executable in ${bin_executables}; do
+    cp ${BUILD_DIR}/build/release/bin/${executable} ${LIB_DIR}/bin-release/
+    cp ${BUILD_DIR}/build/fastdebug/bin/${executable} ${LIB_DIR}/bin-debug/
+done
+
+sbin_executables="kudu-master \
+                  kudu-tserver"
+install -d -m 0755 ${LIB_DIR}/sbin-release
+install -d -m 0755 ${LIB_DIR}/sbin-debug
+for executable in ${sbin_executables}; do
+    cp ${BUILD_DIR}/build/release/bin/${executable} ${LIB_DIR}/sbin-release/
+    cp ${BUILD_DIR}/build/fastdebug/bin/${executable} ${LIB_DIR}/sbin-debug/
+done
+
+# install all the jars except original-, sources, tests, javadocs, and with-dependencies jars
+cp ${BUILD_DIR}/java/target/*.jar ${LIB_DIR}
+cp ${BUILD_DIR}/java/*/target/*.jar ${LIB_DIR}
+rm -f ${LIB_DIR}/original-*.jar
+rm -f ${LIB_DIR}/*-tests.jar
+rm -f ${LIB_DIR}/*-sources.jar
+rm -f ${LIB_DIR}/*-javadoc.jar
+rm -f ${LIB_DIR}/*-with-dependencies.jar
+
+# then create the versionless symlinks for the jars that remain
+# matches, e.g. -1.4.0-cdh5.12.0 or -1.3.1-cdh5.11.3-SNAPSHOT or -1.5.0-cdh5.13.x-beta-3
+versions='s#-[0-9.]\+-cdh[0-9\-\.]*[0-9x]\(-beta-[0-9]\+\)\?\(-SNAPSHOT\)\?##'
+for jar in `find $LIB_DIR -name *.jar`; do
+    versionless=`echo \`basename ${jar}\` | sed -e ${versions}`
+    ln -fs `basename ${jar}` "${LIB_DIR}/${versionless}"
+done
+
+# now, create the defaults files
+install -d -m 0755 ${ETC_DIR}/default
+
+cat > ${ETC_DIR}/default/kudu-master <<__EOT__
+export FLAGS_log_dir=/var/log/kudu
+export FLAGS_rpc_bind_addresses=0.0.0.0:7051
+__EOT__
+chmod 0644 ${ETC_DIR}/default/kudu-master
+
+cat > ${ETC_DIR}/default/kudu-tserver <<__EOT__
+export FLAGS_log_dir=/var/log/kudu
+export FLAGS_rpc_bind_addresses=0.0.0.0:7050
+__EOT__
+chmod 0644 ${ETC_DIR}/default/kudu-tserver
+
+# and the gflagfiles
+install -d -m 0755 $CONF_DIR
+
+cat > ${CONF_DIR}/master.gflagfile <<__EOT__
+# Do not modify these two lines. If you wish to change these variables,
+# modify them in /etc/default/kudu-master.
+--fromenv=rpc_bind_addresses
+--fromenv=log_dir
+
+--fs_wal_dir=/var/lib/kudu/master
+--fs_data_dirs=/var/lib/kudu/master
+__EOT__
+chmod 0644 ${CONF_DIR}/master.gflagfile
+
+cat > ${CONF_DIR}/tserver.gflagfile <<__EOT__
+# Do not modify these two lines. If you wish to change these variables,
+# modify them in /etc/default/kudu-tserver.
+--fromenv=rpc_bind_addresses
+--fromenv=log_dir
+
+--fs_wal_dir=/var/lib/kudu/tserver
+--fs_data_dirs=/var/lib/kudu/tserver
+__EOT__
+chmod 0644 ${CONF_DIR}/tserver.gflagfile
+
+# create wrapper scripts in /usr/bin for user-facing executables
+install -d -m 0755 ${BIN_DIR}
+DO_EXEC="exec "
+for wrapper in ${bin_executables}; do
+  cat > ${BIN_DIR}/${wrapper} <<__EOT__
+#!/bin/bash
+
+export KUDU_HOME=\${KUDU_HOME:-/usr/lib/kudu}
+
+${DO_EXEC}\${KUDU_HOME}/bin/$wrapper "\$@"
+__EOT__
+  chmod 755 ${BIN_DIR}/${wrapper}
+done
+
+# and wrapper scripts in /usr/sbin
+install -d -m 0755 ${SBIN_DIR}
+for wrapper in ${sbin_executables}; do
+  cat > ${SBIN_DIR}/${wrapper} <<__EOT__
+#!/bin/bash
+
+export KUDU_HOME=\${KUDU_HOME:-/usr/lib/kudu}
+
+${DO_EXEC}\${KUDU_HOME}/sbin/$wrapper "\$@"
+__EOT__
+  chmod 755 ${SBIN_DIR}/${wrapper}
+done
+
+install -d -m 0755 ${PREFIX}/var/run/kudu
+install -d -m 0755 ${PREFIX}/var/log/kudu
+install -d -m 0755 ${PREFIX}/var/lib/kudu
+
+install -d -m 0755 ${PREFIX}/${SYSTEM_LIB_DIR}
+install -d -m 0755 ${PREFIX}/${SYSTEM_LIB_DIR}/debug
+install -d -m 0755 ${PREFIX}/${SYSTEM_INCLUDE_DIR}
+
+# Copy in the client libraries and SONAME symlinks. The release file set is
+# copied in directly, while the debug set is placed in a subdirectory.
+for lib in `find ${BUILD_DIR}/build/release/client/usr/local/lib* -name \*.so\*`; do
+  cp -d $lib ${PREFIX}/${SYSTEM_LIB_DIR}
+done
+for lib in `find ${BUILD_DIR}/build/fastdebug/client/usr/local/lib* -name \*.so\*`; do
+  cp -d $lib ${PREFIX}/${SYSTEM_LIB_DIR}/debug
+done
+
+# Doesn't matter whether we use release or fastdebug here; we're not copying binaries.
+cp -r ${BUILD_DIR}/build/release/client/usr/local/include/* ${PREFIX}/${SYSTEM_INCLUDE_DIR}/
+cp -r ${BUILD_DIR}/build/release/client/usr/local/share ${PREFIX}/usr/share
+
+cp -r ${BUILD_DIR}/www ${LIB_DIR}/
+
+# Cloudera specific
+install -d -m 0755 ${LIB_DIR}/cloudera
+cp cloudera/cdh_version.properties ${LIB_DIR}/cloudera/
+
+# Copy the license files, folding the thirdparty licenses into the main file.
+install -d -m 0755 ${PREFIX}/${DOC_DIR}
+cp ${BUILD_DIR}/NOTICE.txt ${PREFIX}/${DOC_DIR}
+cp ${BUILD_DIR}/LICENSE.txt ${PREFIX}/${DOC_DIR}
+echo >> ${PREFIX}/${DOC_DIR}/LICENSE.txt
+echo "Third-party LICENSE.txt follows" >> ${PREFIX}/${DOC_DIR}/LICENSE.txt
+echo "===============================" >> ${PREFIX}/${DOC_DIR}/LICENSE.txt
+cat ${BUILD_DIR}/thirdparty/LICENSE.txt >> ${PREFIX}/${DOC_DIR}/LICENSE.txt
---
+++ kudu.spec
@@ -0,0 +1,199 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+%define kudu_log /var/log/kudu
+%define kudu_run /var/run/kudu
+%define kudu_lib /var/lib/kudu
+%define kudu_doc %{_docdir}/%{name}-%{version}
+
+%if  %{!?suse_version:1}0
+  %define initd_dir %{_sysconfdir}/rc.d/init.d
+  %define alternatives_cmd alternatives
+%else
+  %define initd_dir %{_sysconfdir}/rc.d
+  %define alternatives_cmd update-alternatives
+%endif
+
+Name: kudu
+Version: %{kudu_version}
+Release: %{kudu_release}
+Summary: Columnar storage engine for Hadoop
+URL: http://www.cloudera.com
+Group: Development/Libraries
+Buildroot: %{_topdir}/INSTALL/%{name}-%{version}
+License: ASL 2.0
+Source0: kudu-%{kudu_patched_version}.tar.gz
+Source1: do-component-build
+Source2: install_kudu.sh
+Source3: kudu-master.init
+Source4: kudu-tserver.init
+Requires: cyrus-sasl-plain
+Requires: lsb
+Requires: ntp
+Requires: /usr/sbin/useradd, openssl
+Requires(post): /sbin/chkconfig
+Requires(preun): /sbin/chkconfig
+
+%description
+Columnar storage engine for Hadoop
+
+%package master
+Summary: Kudu Master service
+Group: System/Daemons
+Requires: %{name} = %{version}-%{release}
+
+%description master
+Kudu Master service
+
+%package tserver
+Summary: Kudu Tablet Server service
+Group: System/Daemons
+Requires: %{name} = %{version}-%{release}
+
+%description tserver
+Kudu Tablet Server service
+
+%package client0
+Summary: Kudu client library
+Group: Development/Libraries
+Requires: cyrus-sasl-plain
+
+%description client0
+Kudu client library
+
+%package client-devel
+Summary: Kudu client development package
+Group: Development/Libraries
+Requires: %{name}-client0 = %{version}-%{release}
+
+%description client-devel
+Kudu client development package
+
+# use the debug_package macro if needed
+%if  %{!?suse_version:1}0
+# RedHat does this by default
+%else
+%debug_package
+%endif
+
+%prep
+%setup -n %{name}-%{kudu_patched_version}
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%build
+env FULL_VERSION=%{kudu_patched_version} bash %{SOURCE1}
+
+%install
+%__rm -rf $RPM_BUILD_ROOT
+bash %{SOURCE2} \
+          --build-dir=$PWD \
+          --prefix=$RPM_BUILD_ROOT \
+          --system-include-dir=%{_includedir} \
+          --system-lib-dir=%{_libdir} \
+          --extra-dir=$RPM_SOURCE_DIR \
+          --doc-dir=%{kudu_doc}
+
+# Install init scripts
+init_source=$RPM_SOURCE_DIR
+init_target=$RPM_BUILD_ROOT/%{initd_dir}
+install -d -m 0755 $init_target
+install -m 0755 $init_source/kudu-master.init $init_target/kudu-master
+install -m 0755 $init_source/kudu-tserver.init $init_target/kudu-tserver
+sed -i'' \
+    -e 's/@@SEQ_NUMBERS@@/92 8/' \
+    -e 's/@@CHKCONFIG_LEVELS@@/345/' \
+    -e 's/@@DEFAULT_START@@/3 4 5/' \
+    -e 's/@@DEFAULT_STOP@@/0 1 2 6/' \
+    -e 's/@@NTP_SERVICE@@/ntpd/' \
+    $init_target/kudu-master \
+    $init_target/kudu-tserver
+
+# Install security limits
+#%__install -d -m 0755 $RPM_BUILD_ROOT/etc/security/limits.d
+#%__install -m 0644 %{SOURCE5} $RPM_BUILD_ROOT/etc/security/limits.d/kudu.conf
+
+%pre
+getent group kudu >/dev/null || groupadd -r kudu
+getent passwd kudu >/dev/null || /usr/sbin/useradd --comment "Kudu" --shell /bin/bash -M -r -g kudu --home %{kudu_lib} kudu
+
+%post
+%{alternatives_cmd} --install /etc/kudu/conf kudu-conf /etc/kudu/conf.dist         30
+%{alternatives_cmd} --install /usr/lib/kudu/bin kudu-bin /usr/lib/kudu/bin-release 30
+%{alternatives_cmd} --install /usr/lib/kudu/bin kudu-bin /usr/lib/kudu/bin-debug   20
+%{alternatives_cmd} --install /usr/lib/kudu/sbin kudu-sbin /usr/lib/kudu/sbin-release 30
+%{alternatives_cmd} --install /usr/lib/kudu/sbin kudu-sbin /usr/lib/kudu/sbin-debug   20
+
+%preun
+if [ "$1" = 0 ]; then
+    %{alternatives_cmd} --remove kudu-conf /etc/kudu/conf.dist || :
+    %{alternatives_cmd} --remove kudu-bin /usr/lib/kudu/bin-release || :
+    %{alternatives_cmd} --remove kudu-bin /usr/lib/kudu/bin-debug || :
+    %{alternatives_cmd} --remove kudu-sbin /usr/lib/kudu/sbin-release || :
+    %{alternatives_cmd} --remove kudu-sbin /usr/lib/kudu/sbin-debug || :
+fi
+
+%files
+%defattr(-,root,root)
+/usr/lib/kudu
+/usr/bin/*
+/usr/sbin/*
+%attr(0755,kudu,kudu) %{kudu_log}
+%attr(0755,kudu,kudu) %{kudu_run}
+%attr(0755,kudu,kudu) %{kudu_lib}
+# The %dir directive excludes files within conf.dist in the build tree from
+# leaking into this package.
+%attr(0755,root,root) %config(noreplace) %dir /etc/kudu/conf.dist
+%doc %{kudu_doc}
+#%config(noreplace) /etc/security/limits.d/kudu.conf
+
+%files client0
+%{_libdir}/libkudu_client.so.*
+%{_libdir}/debug/libkudu_client.so.*
+
+%files client-devel
+%{_libdir}/libkudu_client.so
+%{_libdir}/debug/libkudu_client.so
+/usr/include/kudu
+%attr(0755,root,root) /usr/share/doc/kuduClient
+%attr(0755,root,root) /usr/share/kuduClient
+
+%changelog
+
+%define service_macro() \
+\
+%files %1 \
+%attr(0755,root,root)/%{initd_dir}/%2 \
+%attr(0644,root,root) %config(noreplace) /etc/kudu/conf.dist/%1.gflagfile \
+%attr(0644,root,root) %config(noreplace) /etc/default/%2 \
+\
+%post %1 \
+chkconfig --add %2 \
+\
+%preun %1 \
+if [ "$1" = 0 ] ; then \
+    service %2 stop > /dev/null \
+    chkconfig --del %2 \
+fi \
+\
+%postun %1 \
+if [ $1 -ge 1 ]; then \
+    service %2 condrestart >/dev/null 2>&1 || : \
+fi
+
+%service_macro master  kudu-master
+%service_macro tserver kudu-tserver
+
